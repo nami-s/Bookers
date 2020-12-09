f859f388ab73aa4eed6fb210507257c02ca77e4f
nami-s <na.s1222.10feettt@gmail.com>
2020-12-09 03:24:45 +0000

    [add]新規投稿フォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,32 @@
 class BooksController < ApplicationController
+  
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def new
+    @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end  
 
   def edit
   end
+  
+  private
+  # ストロングパラメーター
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end  
 end
+
+
+
+

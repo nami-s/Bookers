f4549fcbfa3eb1040d988d3dd6aab5cb647e45dd
nami-s <na.s1222.10feettt@gmail.com>
2020-12-10 12:43:04 +0000

    [add]validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,22 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
-
-  def new
-    @book = Book.new
-  end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      
+      render "index"
+    end
   end  
 
   def edit
@@ -25,13 +27,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
   end  
   
   def destroy
     book = Book.find(params[:id])
     book.destroy 
-    redirect_to books_path
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end  
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   
-  get 'top' => 'homes#top'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  get '/' => 'homes#top'
+  #get 'books' => 'books#index'
+  #post 'books' => 'books#create'
+  #get 'books/id' => 'books#show'
+  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #patch 'books/:id' => 'books#update'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   
   
 end

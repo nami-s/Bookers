1b46a9a3a14088b05326201972a7ad1b6398c7dc
nami-s <na.s1222.10feettt@gmail.com>
2020-12-12 07:54:43 +0000

    [Finsh]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,9 +25,14 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      @books = Book.all
+      
+      render "edit"
+    end  
   end  
   
   def destroy
